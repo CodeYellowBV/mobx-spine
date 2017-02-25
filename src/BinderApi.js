e2b1b545e15df4a8b8d29f068091e52a1dd3e62a
Kees Kluskens <kees@webduck.nl>
2017-02-25 19:36:49 +0100

    Refactor request handling to BinderApi class

--- src/BinderApi.js
+++ src/BinderApi.js
@@ -1,6 +1,13 @@
 import { mapKeys, mapValues, get } from 'lodash';
+import axios from 'axios';
 import snakeToCamel from './snakeToCamel';
-import request from './request';
+
+// Function ripped from Django docs.
+// See: https://docs.djangoproject.com/en/dev/ref/csrf/#ajax
+function csrfSafeMethod(method) {
+    // These HTTP methods do not require CSRF protection.
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method));
+}
 
 function parseBackendValidationErrors(response) {
     const valErrors = get(response, 'data.error.validation_errors');
@@ -14,11 +21,54 @@ function parseBackendValidationErrors(response) {
 }
 
 export default class BinderApi {
-    baseUrl = '';
+    baseUrl = null;
     csrfToken = null;
+    defaultHeaders = {};
+
+    __request(method, url, data, options) {
+        options || (options = {});
+        const useCsrfToken = csrfSafeMethod(method) ? undefined : this.csrfToken;
+
+        const axiosOptions = {
+            method,
+            baseURL: this.baseUrl,
+            url,
+            data: method !== 'get' && data ? data : undefined,
+            params: method === 'get' && data ? data : undefined,
+            headers: Object.assign({
+                'Content-Type': 'application/json',
+                'X-Csrftoken': useCsrfToken,
+            }, this.defaultHeaders),
+        };
+
+        Object.assign(axiosOptions, options);
+
+        return axios(axiosOptions)
+        .then(response => response.data);
+    }
+
+    get(url, data, options) {
+        return this.__request('get', url, data, options);
+    }
+
+    post(url, data, options) {
+        return this.__request('post', url, data, options);
+    }
+
+    patch(url, data, options) {
+        return this.__request('patch', url, data, options);
+    }
+
+    put(url, data, options) {
+        return this.__request('put', url, data, options);
+    }
+
+    delete(url, data, options) {
+        return this.__request('delete', url, data, options);
+    }
 
     fetchModel({ url, data }) {
-        return request.get(url, data)
+        return this.get(url, data)
         .then((res) => {
             return {
                 data: res.data,
@@ -30,7 +80,7 @@ export default class BinderApi {
 
     saveModel({ url, data, isNew }) {
         const method = isNew ? 'patch' : 'post';
-        return request[method](url, data)
+        return this[method](url, data)
         .catch((err) => {
             if (err.response) {
                 err.valErrors = parseBackendValidationErrors(err.response);
@@ -40,11 +90,12 @@ export default class BinderApi {
     }
 
     deleteModel({ url }) {
-        return request.delete(url);
+        // TODO: kind of silly now, but we'll probably want better error handling soon.
+        return this.delete(url);
     }
 
     fetchStore({ url, data }) {
-        return request.get(url, data)
+        return this.get(url, data)
         .then((res) => {
             return {
                 data: res.data,
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as Model } from './Model';
 export { default as Store } from './Store';
-export { default as request } from './request';
 export { default as BinderApi } from './BinderApi';
