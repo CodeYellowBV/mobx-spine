b848c901e94c4191d92c1b87be9cfc7f146eb043
Kees Kluskens <kees@webduck.nl>
2017-03-23 11:35:34 +0100

    Add `date` cast

--- src/Casts.js
+++ src/Casts.js
@@ -1,6 +1,27 @@
 import moment from 'moment';
 
+function checkMomentInstance(attr, value) {
+    if (!(value instanceof moment)) {
+        throw new Error(`Attribute \`${attr}\` is not a moment instance.`);
+    }
+}
+
 export default {
+    date: {
+        parse(attr, value) {
+            if (value === null) {
+                return null;
+            }
+            return moment.utc(value, 'YYYY-MM-DD');
+        },
+        toJS(attr, value) {
+            if (value === null) {
+                return null;
+            }
+            checkMomentInstance(attr, value);
+            return value.format('YYYY-MM-DD');
+        },
+    },
     datetime: {
         parse(attr, value) {
             if (value === null) {
@@ -12,11 +33,7 @@ export default {
             if (value === null) {
                 return null;
             }
-            if (!(value instanceof moment)) {
-                throw new Error(
-                    `Attribute \`${attr}\` is not a moment instance.`
-                );
-            }
+            checkMomentInstance(attr, value);
             return value.format();
         },
     },
--- src/__tests__/Casts.js
+++ src/__tests__/Casts.js
@@ -69,3 +69,61 @@ test('datetime - should be serialized in toBackend()', () => {
         born_at: '2017-03-22T22:08:23Z',
     });
 });
+
+class Animal2 extends Model {
+    @observable birthDate = null;
+
+    casts() {
+        return {
+            birthDate: Casts.date,
+        };
+    }
+}
+
+test('date - should parse to model', () => {
+    const animal = new Animal2();
+
+    expect(animal.birthDate).toBe(null);
+
+    animal.parse({
+        birthDate: '1995-03-22',
+    });
+
+    expect(animal.birthDate).toBeInstanceOf(moment);
+    expect(animal.birthDate.format()).toBe('1995-03-22T00:00:00Z');
+});
+
+test('date - should parse to model when null', () => {
+    const animal = new Animal2({ birthDate: '1995-03-22' });
+    expect(animal.birthDate).toBeInstanceOf(moment);
+
+    animal.parse({
+        birthDate: null,
+    });
+
+    expect(animal.birthDate).toBe(null);
+});
+
+test('date - parse() should throw away time info', () => {
+    const animal = new Animal2({ birthDate: '2017-03-22T22:08:23+00:00' });
+
+    expect(animal.birthDate.format()).toBe('2017-03-22T00:00:00Z');
+});
+
+test('date - should be serialized in toJS()', () => {
+    const animal = new Animal2({ birthDate: '1995-03-22' });
+
+    expect(animal.toJS()).toEqual({
+        birthDate: '1995-03-22',
+    });
+});
+
+test('date - toJS() should throw error when moment instance is gone', () => {
+    const animal = new Animal2({ birthDate: '1995-03-22' });
+
+    animal.birthDate = 'asdf';
+
+    expect(() => {
+        return animal.toJS();
+    }).toThrow('Attribute `birthDate` is not a moment instance.');
+});
