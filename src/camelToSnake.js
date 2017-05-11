205ed7d812fc7f12876b8190848f53614ee50982
Kees Kluskens <kees@webduck.nl>
2017-05-11 15:49:00 +0200

    remove unused piece of code

--- src/camelToSnake.js
+++ src/camelToSnake.js
@@ -1,7 +1,4 @@
 // lodash's `snakeCase` method removes dots from the string; this breaks mobx-spine
 export default function camelToSnake(s) {
-    if (s.startsWith('_')) {
-        return s;
-    }
     return s.replace(/([A-Z])/g, $1 => '_' + $1.toLowerCase());
 }
